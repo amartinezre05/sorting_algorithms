94de1531ff1eac5557ae734fcb168ef57662ba16
jackeado <pabsanchez@gmail.com>
2020-02-03 18:17:25 -0500

    fix output

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,13 +23,13 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
+	    min = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
 		}
-		if (j != min)
+		if (array[i] != array[min])
 		{
 			swap = array[i];
 			array[i] = array[min];
