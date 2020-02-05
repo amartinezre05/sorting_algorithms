de18e1d0a59aa55c8bf11192561cfe462abcf10a
jackeado <pabsanchez@gmail.com>
2020-02-05 15:12:26 -0500

    fix bug

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,9 +11,7 @@
  * Return: Void
  */
 
-
 void selection_sort(int *array, size_t size)
-
 {
 	size_t i, j, min;
 	int swap;
@@ -23,7 +21,7 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-	    min = i;
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
