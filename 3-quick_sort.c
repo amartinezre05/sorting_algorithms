f63548cc4c445440ee34906c039df39c936d9847
jackeado <pabsanchez@gmail.com>
2020-02-05 19:51:58 -0500

    fix betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,38 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
 #include <stddef.h>
+#include "sort.h"
+
+/**
+ * partition - function to locate the pivot and divide it
+ * @array: array to sort
+ * @low: lower value of index to sort
+ * @hi: higest value
+ * @size: size of array
+ * Return: index of partition
+ */
 
 size_t partition(int *array, ssize_t low, ssize_t hi, size_t size)
-
 {
-    ssize_t i, j;
-    int pivot, swap;
+	ssize_t i, j;
+	int pivot, swap;
 
-    pivot = array[hi];
-    i = (low -1);
-    for (j = low; j <= hi - 1; j++)
-    {
-        if (array[j] < pivot) {
-            i++;
-            if (i != j) {
-                swap = array[i];
-                array[i] = array[j];
-                array[j] = swap;
-                print_array(array, size);
-            }
-        }
-    }
-    if (array[hi] < array[i + 1])
-    {
-        swap = array[i + 1];
-        array[i + 1] = array[hi];
-        array[hi] = swap;
-        print_array(array, size);
-    }
-    return (i + 1);
+	pivot = array[hi];
+	i = low - 1;
+	for (j = low; j <= hi - 1; j++)
+	{
+
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				printf("pivot: %d, iterador: %ld, j: %ld, \n", pivot, i, j);
+				swap = array[i];
+				array[i] = array[j];
+				array[j] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[hi] < array[i + 1])
+	{
+		swap = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = swap;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
-
+/**
+ * re_quick_sort - function that recursive algorithm
+ * @array: array to sorted
+ * @low: lowest value
+ * @hi: higest value
+ * @size: size of array
+ */
 
 void re_quick_sort(int *array, ssize_t low, ssize_t hi, size_t size)
 {
@@ -46,10 +64,15 @@ void re_quick_sort(int *array, ssize_t low, ssize_t hi, size_t size)
 	}
 }
 
+/**
+ * quick_sort - sorts an array of int in ascending order using the Quick sort
+ * @array: array to sort
+ * @size: size of array
+ */
 
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-    re_quick_sort(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	re_quick_sort(array, 0, size - 1, size);
 }
